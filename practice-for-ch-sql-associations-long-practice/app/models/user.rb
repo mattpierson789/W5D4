b4cce8e00db762ee69ca8739749678454e61d83c
Matt Pierson <matt.pierson789@gmail.com>
2023-04-06 15:36:53 -0400

    fixing set-up

---
+++ practice-for-ch-sql-associations-long-practice/app/models/user.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  name       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class User < ApplicationRecord
+
+    # has_many :enrollments
+    #     primary_key: :id,
+    #     foreign_key:  
+
+
+    
+
+    
+end
